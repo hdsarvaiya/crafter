def0b0e59dc42f6b9e61ccc3ff88422677bc6a00
Harshvardhansinh Sarvaiya <145086716+hdsarvaiya@users.noreply.github.com>
2024-10-07 13:50:58 +0530

    Update page.js

--- pages/buildArea/page.js
+++ pages/buildArea/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import Builder from '@/app/Builder/page';
+import Builder from '@/pages/Builder/page';
 
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
